f1d54727d7f213c9f175badb864a58a972a2945e
Ale <ma.salazarsanchez@gmail.com>
2017-10-09 22:01:54 -0300

    Last value repeated issue fixed

--- fibonacci.js
+++ fibonacci.js
@@ -4,21 +4,11 @@
 //and tell the array to consider 0 and 1 as the first 2 items.
 var fib = [0,1];
 
-//The "n" variable is the one that when altered, prints in the Console the first
-// number of items of the array that I want to see.
-var n = fib.length;
-n = 10;
-
-//for loop that adds to the array the following numbers of the fibonacci sequence.
-for ( i = 2 ; i < n ; i++ ) {
-
+//For loop that adds to the array the following numbers of the fibonacci sequence.
+//When the length is altered it returns that many numbers from the sequence.
+for ( i = 2 ; i < (fib.length = 15) ; i++ ) {
   fib[i] = fib[i-1] + fib[i-2];
   fib.push(fib[i]);
-
-  //due to an error that repats the last item in the list,
-  //I removed it with the splice method.
-  fib.splice(-1);
-
 }
 
 //I tell the Console to print the array.
