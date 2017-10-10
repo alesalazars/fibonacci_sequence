1ae316c173a0bde24674574e79927ca85259eb0d
Ale <ma.salazarsanchez@gmail.com>
2017-10-09 22:36:39 -0300

    Logic didn't make sense. Fixed.

--- fibonacci.js
+++ fibonacci.js
@@ -4,11 +4,13 @@
 //and tell the array to consider 0 and 1 as the first 2 items.
 var fib = [0,1];
 
+//"n" is the variable that determines the amount of elements of the array that will show up later.
+var n = fib.length;
+n = 15;
+
 //For loop that adds to the array the following numbers of the fibonacci sequence.
-//When the length is altered it returns that many numbers from the sequence.
-for ( i = 2 ; i < (fib.length = 15) ; i++ ) {
+for ( i = 2 ; i < n ; i++ ) {
   fib[i] = fib[i-1] + fib[i-2];
-  fib.push(fib[i]);
 }
 
 //I tell the Console to print the array.
