9ebb787655f9e147dff85e1a6dec56a1157f16f3
Ale <ma.salazarsanchez@gmail.com>
2017-10-09 23:21:38 -0300

    Correct logic

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,17 @@
 /* ====== PRINT "N" FIRST NUMBERS OF THE FIBONACCI SEQUENCE ======*/
 
-//I declare the variable fib (for fibonacci)
-//and tell the array to consider 0 and 1 as the first 2 items.
+// I declare the variable fib (for Fibonacci)
+// and tell the array to consider 0 and 1 as the first 2 items.
 var fib = [0,1];
 
-//"n" is the variable that determines the amount of elements of the array that will show up later.
-var n = fib.length;
-n = 15;
+// "n" is the variable that determines the amount of elements of the array
+// that will show up later.
+n = 10;
 
-//For loop that adds to the array the following numbers of the fibonacci sequence.
+// It iterates "n" number of times adding new elements to the array
+// forming the Fibonacci sequence.
 for ( i = 2 ; i < n ; i++ ) {
-  fib[i] = fib[i-1] + fib[i-2];
+  fib[i] = fib[i-1] + fib[i-2]; // The new element is always the sum of the previous 2.
 }
 
 //I tell the Console to print the array.
